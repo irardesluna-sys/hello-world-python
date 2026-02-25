7fa71366a60a4847335322e957e1b5f8344c74f8
irardesluna-sys <irardes.luna@souunit.com.br>
2026-02-25 14:11:24 +0000

    Update 6.1

--- microexercicio6.py
+++ microexercicio6.py
@@ -1,5 +1,5 @@
-nota1 = float(input("Inserir nota 1 "))
-nota2 = float(input("Inserir nota 2 "))
+nota1 = float(input("Inserir nota 1: "))
+nota2 = float(input("Inserir nota 2: "))
 media = (nota1 + nota2)/2
 
 if media >= 7:
@@ -8,4 +8,3 @@ elif media >=4 < 7:
     print("Prova Final")
 else:
     print("Reprovado")
-    
