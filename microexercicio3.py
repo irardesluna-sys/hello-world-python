ee2355b662d302de961f77a2a60472c766d30c2b
irardesluna-sys <irardes.luna@souunit.com.br>
2026-02-25 13:05:18 +0000

    New updates

---
+++ microexercicio3.py
@@ -0,0 +1,18 @@
+dia_da_semana = int(input("Digite o número do dia da semana (1-7): "))
+
+if dia_da_semana == 1:
+    print("Segunda-feira")
+elif dia_da_semana == 2:
+    print("Terça-feira")
+elif dia_da_semana == 3:
+    print("Quarta-feira")
+elif dia_da_semana == 4:
+    print("Quinta-feira")
+elif dia_da_semana == 5:
+    print("Sexta-feira")
+elif dia_da_semana == 6:
+    print("Sábado")
+elif dia_da_semana == 7:
+    print("Domingo")
+else:
+    print("Dia inválido")
