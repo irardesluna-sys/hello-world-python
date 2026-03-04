4215d4759d04a583a06239ac740aa7557acd8a88
irardesluna-sys <irardes.luna@souunit.com.br>
2026-03-04 12:16:50 +0000

    microexercicio08

---
+++ microexercicio08.py
@@ -0,0 +1,8 @@
+pontuacao = float(input("Qual a sua pontuação: "))
+
+if pontuacao >= 90:
+    pass #ToDO: implementar a lógica
+elif pontuacao >= 70:
+    print("Bom desempenho!")
+else:
+    print("Precisa melhorar.")
